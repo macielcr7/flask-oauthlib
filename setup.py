b53b302c83205d90d2dafab62e664c466af46e3d
Maciel Sousa <macielcr7@gmail.com>
2025-05-23 12:20:08 -0300

    fix: update oauthlib version requirement to >=3.2.2

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     license='BSD',
     install_requires=[
         'Flask',
-        'oauthlib>=3.0.0,<3.2.2',
+        'oauthlib>=3.2.2',
         'requests-oauthlib>=1.0,<1.3.1',
         'cachelib',
     ],
